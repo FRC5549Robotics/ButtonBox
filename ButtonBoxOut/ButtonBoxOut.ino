b50a9751964647d1105b719f62ff14c2afe12504
TyrannousHail03 <TyrannousHail03@users.noreply.github.com>
2019-02-12 16:30:31 -0500

    Button Box Test 1
    First implementation of the Button Box code.

---
+++ ButtonBoxOut/ButtonBoxOut.ino
@@ -0,0 +1,40 @@
+
+#include <Joystick.h>
+
+Joystick_ Joystick;
+
+void setup() {
+  // Initialize Button Pins
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+
+  // Initialize Joystick Library
+  Joystick.begin();
+}
+
+// Constant that maps the phyical pin to the joystick button.
+const int pinToButtonMap = 5;
+
+// Last state of the button
+int lastButtonState[7] = {0,0,0,0,0,0,0};
+
+void loop() {
+
+  // Read pin values
+  for (int index = 0; index < 7; index++)
+  {
+    int currentButtonState = !digitalRead(index + pinToButtonMap);
+    if (currentButtonState != lastButtonState[index])
+    {
+      Joystick.setButton(index, currentButtonState);
+      lastButtonState[index] = currentButtonState;
+    }
+  }
+
+  delay(50);
+}
