5695cd3e86dcb1aa51c6bf41401309fb475b551e
TyrannousHail03 <TyrannousHail03@users.noreply.github.com>
2019-02-27 15:13:49 -0500

    Added Comments and Added Throttle
    Added a function for throttle as well as added a multi-line comment detailing the button box layout. Also fixed

--- ButtonBoxOut/ButtonBoxOut.ino
+++ ButtonBoxOut/ButtonBoxOut.ino
@@ -3,6 +3,8 @@
 
 Joystick_ Joystick;
 
+int throttleNum = A0;
+
 void setup() {
   // Initialize Button Pins
   pinMode(2, INPUT_PULLUP);
@@ -12,19 +14,21 @@ void setup() {
   pinMode(6, INPUT_PULLUP);
   pinMode(7, INPUT_PULLUP);
   pinMode(8, INPUT_PULLUP);
-
+  pinMode(A0, INPUT_PULLUP);
+  
+  Joystick.setThrottleRange(0, 255);
+  
   // Initialize Joystick Library
   Joystick.begin();
 }
 
 // Constant that maps the phyical pin to the joystick button.
-const int pinToButtonMap = 5;
+const int pinToButtonMap = 2;
 
 // Last state of the button
 int lastButtonState[7] = {0,0,0,0,0,0,0};
 
-void loop() {
-
+void loop() {  
   // Read pin values
   for (int index = 0; index < 7; index++)
   {
@@ -36,5 +40,20 @@ void loop() {
     }
   }
 
+  Joystick.setThrottle(analogRead(throttleNum));
+
   delay(50);
 }
+
+/* Button Layout for FRC Deep Space Challenge:
+ *  Top Black: Button 1 - Pin 2
+ *  Red: Button 2 - Pin 3
+ *  Middle Black: Button 3 - Pin 4
+ *  Yellow: Button 4 - Pin 5
+ *  Bottom Black: Button 5 - Pin 6
+ *  Green: Button 6 - Pin 7
+ *  Vision Assist Button: Button 7 - Pin 8
+ *  
+ * Throttle:
+ *  Analog - Pin A1
+ */
